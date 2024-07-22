5c9e70a7b554ce68d35d8b4c2dda783adec69db4
FarazAhmad2 <farazahmad574@gmail.com>
2024-07-22 21:54:50 +0530

    second commit

--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -1,24 +1,13 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 
-const items = [
-  { id: 1, name: "Item 1", description: "Description of Item 1" },
-  { id: 2, name: "Item 2", description: "Description of Item 2" },
-  { id: 3, name: "Item 3", description: "Description of Item 3" },
-];
-
 const ItemDetail = () => {
   const { id } = useParams();
-  const item = items.find((item) => item.id === parseInt(id));
-
-  if (!item) {
-    return <div>Item not found</div>;
-  }
 
   return (
     <div>
-      <h1>{item.name}</h1>
-      <p>{item.description}</p>
+      <h1>{`Item ${id}`}</h1>
+      <p>{`Description for Item ${id}`}</p>
     </div>
   );
 };
--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -2,9 +2,9 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const items = [
-  { id: 1, name: "Item 1", description: "Description of Item 1" },
-  { id: 2, name: "Item 2", description: "Description of Item 2" },
-  { id: 3, name: "Item 3", description: "Description of Item 3" },
+  { id: 1, name: "Item 1" },
+  { id: 2, name: "Item 2" },
+  { id: 3, name: "Item 3" },
 ];
 
 const ItemList = () => {
